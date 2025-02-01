49effe6e092d5be902449fff34ff28ab565aecae
Jasmeet Singh <singhjasmeet200526@gmail.com>
2025-02-02 01:28:45 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/database.js';
-import userRoutes from './routes/userRoutes.js';
-import taskRoutes from './routes/taskRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoutes from "./routes/userRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 
@@ -14,12 +14,13 @@ app.use(express.json());
 // Connect to MongoDB
 connectDB();
 
-// Routes 
-app.use('/api/users', userRoutes);
-app.use('/api/tasks', taskRoutes);
+// Routes
+app.get("/", (req, res) => res.send("Hello from the backend"));
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
